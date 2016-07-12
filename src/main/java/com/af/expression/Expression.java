8f65305f723120cd1f6819e2f3dd3ded7cde1c6c
hns1971 <524395609@qq.com>
2016-07-12 16:20:06 +0800

    [build] 1.1.16

--- src/main/java/com/af/expression/Expression.java
+++ src/main/java/com/af/expression/Expression.java
@@ -8,8 +8,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.af.util.MehodSignatureMatcher;
 
